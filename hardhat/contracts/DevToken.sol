d4d6df6fca6e52f9c8ced7ae6ae3aeba76281f42
route-2 <ruthurao@gmail.com>
2023-02-01 02:05:56 +0530

    contract

--- hardhat/contracts/DevToken.sol
+++ hardhat/contracts/DevToken.sol
@@ -25,6 +25,26 @@ function mint(uint256 amount) public payable
     require(totalSupply() + amountWithDecimals <= maxTotalSupply, "Max supply reached");
     _mint(msg.sender, amountWithDecimals);
 }
+
+function claim() public {
+    address sender = msg.sender;
+    uint256 balance = DevsNFT.balanceOf(sender);
+    require(balance>0,"you dont own any nft");
+    uint256 amount = 0;
+
+    for(uint256 i = 0; i < balance; i++){
+        uint256 tokenId = DevsNFT.tokenOfOwnerByIndex(sender, i);
+        // if nft not claimed then increment amount
+        if(!tokenIdsClaimed[tokenId]){
+            amount += 1;
+            tokenIdsClaimed[tokenId] = true;
+        }
+    }
+
+    require(amount > 0, "You have already claimed your tokens");
+    _mint(msg.sender, amount * tokensPerNFT);
+
+}
      
 
   }
